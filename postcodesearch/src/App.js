54c2ec5b7718286fedf8a549a3eef103c386ed51
Jake Leigh <jake@jakeleigh.io>
2021-03-15 18:41:45 +0000

    add some basic setup styling and a form input button combo

--- postcodesearch/src/App.js
+++ postcodesearch/src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Postcode Searcher</h1>
+      <div className='container'>
+        <div className='titleDiv'>
+          <h1>Welcome to Postcode Searcher</h1>
+        </div>
+        <div className='form'>
+          <form>
+            <input type='text' placeholder='Enter Postcode' />
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
