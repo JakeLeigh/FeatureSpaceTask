5a8f386e337c138d44292363f66097d47d29965c
Jake Leigh <jake@jakeleigh.io>
2021-03-15 19:38:03 +0000

    setup table columns

--- postcodesearch/src/App.js
+++ postcodesearch/src/App.js
@@ -39,31 +39,18 @@ function App() {
         <table class="table table-striped table-dark">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Postcode</th>
+              <th scope="col">Country</th>
+              <th scope="col">Region</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <th scope="row">CB4 0GF</th>
+              <td>England</td>
+              <td>East Of England</td>
             </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
+           
           </tbody>
         </table>
         </div>
