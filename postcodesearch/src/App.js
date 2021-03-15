d4800c44b556ebbd328c007f76ab25464569b517
Jake Leigh <jake@jakeleigh.io>
2021-03-15 19:36:22 +0000

    create second comp to show nearest postcode data and add bootstrap table

--- postcodesearch/src/App.js
+++ postcodesearch/src/App.js
@@ -23,15 +23,49 @@ function App() {
         <div className='titleDiv'>
           <h1>Welcome to Postcode Searcher</h1>
         </div>
-        <div className='form'>
+        <div className='form comp'>
+          <h3>Enter Postcode</h3>
           <form>
-            <input type='text' placeholder='Enter Postcode' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+            <input type='text' placeholder='CB4 0GF' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
             <button type="submit" className="btn btn-primary" onClick={callApi}>Submit</button>
           </form>
           <div className='resultsDiv'>
             <h5>Country: <span id='country'>{results.country}</span></h5>
             <h5>Region: <span id='region'>{results.region}</span></h5>
           </div>
+        </div>
+        <div className='comp relatedPostcodes'>
+          <h3>Nearest Postcodes</h3>
+        <table class="table table-striped table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
         </div>
       </div>
     </div>
