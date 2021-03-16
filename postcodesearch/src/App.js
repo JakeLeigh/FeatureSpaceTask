10d13b8833fcd73795bdd538bce09766348b01bd
Jake Leigh <jake@jakeleigh.io>
2021-03-16 16:05:00 +0000

    add comments for refactored application

--- postcodesearch/src/App.js
+++ postcodesearch/src/App.js
@@ -1,13 +1,18 @@
+//import react and usestate libraries
 import React, {useState} from 'react';
-import axios from 'axios';
+//import css file
 import './App.css';
-import { useHistory, Link, Route, BrowserRouter as Router} from 'react-router-dom';
+//import packages from react-router-dom in order to create successful routes and components
+import {Link, Route, BrowserRouter as Router} from 'react-router-dom';
+//import postcode component
 import Postcode from './components/Postcode';
 
 function App() {
+  //define inital state props for search term and error message
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const test = () => {
+  //method to check whether search term value is empty when button clicked, if so then update error message to inform user to enter a postcode and make sure it is valid
+  const checkSearchTerm = () => {
     if(searchTerm == ''){
       setErrorMessage('Please enter a postcode and make sure it is valid');
     }
@@ -19,12 +24,17 @@ function App() {
         <div className='form comp'>
           <h3>Enter Postcode</h3>
           <form className="was-validated" data-toggle="validator">
+            {/* create text input where value is set to searchTerm state value and onchange can update value of searchterm using e.target.value*/}
             <input type='text' placeholder='CB4 0GF' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} required="required"/>
+            {/* div to hold error message*/}
               <div className="error">{errorMessage}</div>
-            <Link to={`/${searchTerm}`} onClick={test} className="btn btn-primary btn-sm submitButton">Submit</Link>
+              {/*Link component that redirects to /postcode and performs the onclick check to see if the searchterm value is empty or not */}
+            <Link to={`/${searchTerm}`} onClick={checkSearchTerm} className="btn btn-primary btn-sm submitButton">Submit</Link>
           </form>
         </div>
         <div>
+          {/* Define a component route that renders the Postcode comp which is passed props, and the setErrormessage usestate value in order to update the error message in the component itself*/}
+          {/* :postcode is a wildcard to say any value after the initial /... should redirect to postcode comp*/}
           <Route path='/:postcode' render={(props) => <Postcode {...props} errorMessage={setErrorMessage} />}/>
         </div>
       </div>
--- postcodesearch/src/components/Postcode.js
+++ postcodesearch/src/components/Postcode.js
@@ -1,20 +1,31 @@
+//import react, usestate and useeffect packages from react
 import React, {useState, useEffect} from 'react';
+//import css file
 import '../App.css';
+//import axios lib
 import axios from 'axios';
 
 function Postcode(props){
+  //using the props value that was passed through by Route, create a const var that holds the value of the url query param postcode
   const postcode = props.match.params.postcode;
+  //define state props for results and nearest results
   const [results, setResults] = useState({});
   const [nearestResults, setNearestResults] = useState([]);
 
+  //useeffect method gets called once the component renders, I have also attached the postcode const to this method which means useEffect will watch that var and if the value changes, it will run the method again
   useEffect(() => {
+    //if the postcode value matches the government provided regex formula for UK postcodes, then call apis
     if(postcode.match('^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$') != null){
+      //reset the error message to an empty string in order to hide it
       props.errorMessage('');
+      //axios get request using string interpolation to append the postcode value to the url
       axios.get(`http://api.postcodes.io/postcodes/${postcode}`)
       .then(res => {
+        //if the request was successful and we get back a response, then store the value of data.result in the results state object
         setResults(res.data.result);
       })
       .catch(err => {
+        //if the api failed for any reason, then update the error message to inform the user there has been some sort of server error
         props.errorMessage('There has been an internal server error, please try again later');
       })
       
@@ -29,6 +40,7 @@ function Postcode(props){
     else {
       props.errorMessage('Please enter a postcode and make sure it is valid');
     }
+    //watch the postcode var, if it changes then call this method again
   }, [postcode]);
     return (
       <div>
@@ -47,6 +59,7 @@ function Postcode(props){
             </tr>
           </thead>
           <tbody>
+            {/* map through the array list of nearest results and create a new table row for each element and output postcode, country, region*/}
             {nearestResults.map((result, i) => {
               return (
                 <tr key={i}>
